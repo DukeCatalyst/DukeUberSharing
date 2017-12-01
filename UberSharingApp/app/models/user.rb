abbe7ddabd61965e83f0b8eeb2cace4e4856c656
BillGatesNephew <wlkrwilletts@gmail.com>
2017-12-01 17:14:36 -0500

    Added basic validation for the user sign-up. Requires Duke email for signup now

--- UberSharingApp/app/models/user.rb
+++ UberSharingApp/app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable 
+
+
+  # Email Validation 
+  validates :email, format: {
+    with: /\A((.*)(@duke.edu(\s*)))\z/,
+    message: "Must be an @duke.edu email"
+  }, presence: true
+
+  # First Name Validation (Allows hyphens)
+  validates :first_name, format: {
+    with: /\A(([^\d\W]*)|(([^\d\W]*)-([^\d\W]*)))\z/,
+    message: "must be composed only of alphabetical characters."
+  }, presence: true
+
+  # Last Name Validation (Allows hyphens)
+  validates :first_name, format: {
+    with: /\A(([^\d\W]*)|(([^\d\W]*)-([^\d\W]*)))\z/,
+    message: "must be composed only of alphabetical characters."
+  }, presence: true
+
+  # Phone Number Validation
+  validates :phone, format: {
+    with: /\A(\d{3}-\d{3}-\d{4})\z/,
+    message: "Phone number must be of the form: 123-456-5555"
+  }, presence: true
+
 end
